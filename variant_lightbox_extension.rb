f34849f14d36703676cce6b544d96b721b876ae0
Stephanie Powell <steph@The-Laptop.(none)>
2009-02-05 20:21:31 -0700

    Adding details for extension.

--- variant_lightbox_extension.rb
+++ variant_lightbox_extension.rb
@@ -3,19 +3,9 @@
 
 class VariantLightboxExtension < Spree::Extension
   version "1.0"
-  description "Describe your extension here"
-  url "http://yourwebsite.com/variant_lightbox"
+  description "Variant Lightbox Extension"
+  url "http://www.endpoint.com"
 
-  # Please use variant_lightbox/config/routes.rb instead for extension routes.
-
-  # def self.require_gems(config)
-  #   config.gem "gemname-goes-here", :version => '1.2.3'
-  # end
-  
   def activate
-    # admin.tabs.add "Variant Lightbox", "/admin/variant_lightbox", :after => "Layouts", :visibility => [:all]
-    Product.class_eval do
-      #Override thumbnail view here
-    end
   end
 end
