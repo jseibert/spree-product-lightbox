ac315ebd5d6e77aaf0283b1733b6cad2caf19ae6
Stephanie Powell <steph@The-Laptop.(none)>
2009-02-09 16:29:27 -0700

    Removing border of images. Renaming extension. Attempting to fix image size render.

--- product_lightbox_extension.rb
+++ product_lightbox_extension.rb
@@ -1,9 +1,9 @@
 # Uncomment this if you reference any of your controllers in activate
 # require_dependency 'application'
 
-class VariantLightboxExtension < Spree::Extension
+class ProductLightboxExtension < Spree::Extension
   version "1.0"
-  description "Variant Lightbox Extension"
+  description "Product Lightbox Extension"
   url "http://www.endpoint.com"
 
   def activate
